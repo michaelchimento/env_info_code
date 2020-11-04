29b73f9b112dce90983265a2ba077e55803e4109
mchimento@ab.mpg.de <mchimento@ab.mpg.de>
2020-11-04 16:13:14 +0100

    added lux support

--- record_env.py
+++ record_env.py
@@ -1,18 +1,24 @@
 import datetime
-import board
 import busio
-import adafruit_si7021
 import os
-import time
 import sys
+import time
+import board
+import adafruit_bh1750
+import adafruit_si7021
+
+ 
+i2c = board.I2C()
  
 # Create library object using our Bus I2C port
-i2c = busio.I2C(board.SCL, board.SDA)
 
 try:
-    sensor = adafruit_si7021.SI7021(i2c)
+    sensor = adafruit_bh1750.BH1750(i2c) #lux sensor
+    sensor2 = adafruit_si7021.SI7021(i2c) #temp/ humidity sensor
+
+
 except Exception as e:
-    print("problem with sensor")
+    print("problem with sensor: {}".format(e))
     sys.exit()
 
 location="Gblock1"
@@ -20,11 +26,11 @@ location="Gblock1"
 
 time_stamp = datetime.datetime.now()
 print("program is starting at {}".format(time_stamp))
-file_name = "/home/pi/env_info_code/data/env_info_{}.csv".format(time_stamp.date())
+file_name = "/home/pi/env_info_code/data/lux_info_{}.csv".format(time_stamp.date())
 
 if not os.path.isfile(file_name):
    with open(file_name, "a") as savefile:
-       header = "timestamp,location,temperature,humidity\n"
+       header = "timestamp,location,lux\n"
        savefile.write("#{} start time: {} \n".format(location,time_stamp))
        savefile.write(header)
 else:
@@ -32,11 +38,16 @@ else:
         savefile.write("#{} start time: {} \n".format(location,time_stamp))
 
 while __name__=="__main__":
-    current_temp = sensor.temperature
-    current_humidity = sensor.relative_humidity
+    
+    #read from lux sensor
+    current_lux = sensor.lux
+    
+    #read from temp humidity sensor
+    current_temp = sensor2.temperature
+    current_humidity = sensor2.relative_humidity
     time_stamp = datetime.datetime.now()
-    write_line = "{},{},{},{}\n".format(time_stamp,location,current_temp,current_humidity)
+    write_line = "{},{},{},{},{}\n".format(time_stamp,location,current_lux,current_temp,current_humidity)
     print(write_line)
     with open(file_name, "a") as savefile: # open data file in write mode
         savefile.write(write_line)
-    time.sleep(600)
+    time.sleep(1)
